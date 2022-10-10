33e82999b83f66e4028cf021b8313e1bec8a8f81
hide-code <kitahide13@icloud.com>
2022-10-10 15:59:22 +0900

    feat: 一覧ページからコンテント詳細ページに遷移できる機能追加

--- resources/views/content/index.blade.php
+++ resources/views/content/index.blade.php
@@ -64,10 +64,10 @@
               @foreach ($contents as $content)
               <div class="p-4 md:w-1/3">
                 <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                  <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="{{ asset($content->avatar) }}" alt="blog">
+                  <a href="{{ route('content.show', ['id' => $content->id]) }}"><img class="lg:h-48 md:h-36 w-full object-cover object-center" src="{{ asset($content->avatar) }}" alt="blog"></a>
                   <div class="p-6">
                     <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">@foreach ($content->categories as $category) {{ $category->name }} @endforeach</h2>
-                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">{{ $content->title }}</h1>
+                    <a href="{{ route('content.show', ['id' => $content->id]) }}"><h1 class="title-font text-lg font-medium text-gray-900 mb-3">{{ $content->title }}</h1></a>
                     <p class="leading-relaxed mb-3">{{ $content->coment }}</p>
                     <div class="flex items-center flex-wrap ">
                       <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More

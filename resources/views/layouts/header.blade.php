5a0d73c8d7ae96f6cf8653b11a6450b288685bd0
hide-code <kitahide13@icloud.com>
2023-02-03 12:50:29 +0900

    refactor: コンテンツページのフォント調整

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -13,13 +13,12 @@
   <header class="text-gray-600 body-font">
     <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
       <a href="{{ route('content.index') }}" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+        <svg xmlns="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Hollywood_Sign_%28Zuschnitt%29.jpg/240px-Hollywood_Sign_%28Zuschnitt%29.jpg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
           <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
         </svg>
-        <span class="ml-3 text-xl">SwithPlus</span>
+        <span class="ml-3 text-xl">With Movie</span>
       </a>
-
-      <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+            <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
         @auth
           <form method="POST" action="{{ route('logout') }}">
             @csrf

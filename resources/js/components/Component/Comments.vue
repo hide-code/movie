77e9c5d1372b3aae78825e76c90d011b4c493c2b
hide-code <kitahide13@icloud.com>
2023-02-11 20:20:52 +0900

    refactor: バージョニング

--- resources/js/components/Component/Comments.vue
+++ resources/js/components/Component/Comments.vue
@@ -6,12 +6,12 @@
         <div class="mb-6">
           <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">コメントタイトル</label>
           <input v-model="title" type="text" id="default-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-          <p v-if="valid.title" class="mt-2 text-sm text-red-600 dark:text-red-500">20文字以上40文字以内で入力してください。</p>
+          <p v-if="valid.title" class="mt-2 text-sm text-red-600 dark:text-red-500">50文字以内で入力してください。</p>
         </div>
         <div class="mb-6">
           <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">コメント</label>
-          <textarea v-model="content" id="message" name="content" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
-          <p v-if="valid.content" class="mt-2 text-sm text-red-600 dark:text-red-500">20文字以上200文字以内で入力してください。</p>
+          <textarea v-model="content" id="message" name="content" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
+          <p v-if="valid.content" class="mt-2 text-sm text-red-600 dark:text-red-500">200文字以内で入力してください。</p>
         </div>
         <button type="submit" @click="post" class="text-black bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">投稿</button>
       </div>
@@ -49,10 +49,10 @@
     },
     methods: {
       isInValidTitle() {
-        return this.title.length === 0 || this.content.length > 5
+        return this.title.length === 0 || this.title.length > 50
       },
       isInValidContent() {
-        return this.content.length === 0 || this.content.length > 5
+        return this.content.length === 0 || this.content.length > 200
       },
       post: function () {
         this.valid.title = this.isInValidTitle()
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,8 +5,8 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta name="csrf-token" content="{{ csrf_token() }}">
-   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-   <script src="{{ asset('/js/app.js') }}" defer></script>
+   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+   <script src="{{ mix('/js/app.js') }}" defer></script>
    <title>@yield('title')</title>
 </head>
 <body>
